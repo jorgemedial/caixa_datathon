acde23eddebf6c024fcd616618eff1873f923d61
jorgemedial <jorgemedial@gmail.com>
2022-10-14 14:05:04 +0200

    target provisional

--- lib/data_cleasing_tools.py
+++ lib/data_cleasing_tools.py
@@ -2,8 +2,27 @@ import pandas as pd
 from config.constants import application_record_path, credit_score_path, application_record_clean_path
 
 
-def cleanse_data(df: pd.DataFrame):
-    _get_dummies(df)
+def cleanse_data(app: pd.DataFrame, cred: pd.DataFrame):
+    _get_dummies(app)
+    app = app.merge(_get_delayed(cred).reset_index(), on=['ID'], how='left')
+    pass
+def score(label):
+    score = {"1": 0.33, "2": 0.66, "3": 1, "4": 1, "5":1}
+    try:
+        return score[label]
+    except:
+        return 0
+
+def _get_delayed(df):
+    cred["delay"] = cred["STATUS"].map(score)
+    cred.set_index("ID", inplace=True)
+    scores_per_id = cred.groupby(level=0).max()
+    return scores_per_id["delay"]
+
+def _pivot(df_cred_rec: pd.DataFrame):
+    df_target = df_cred_rec.pivot(values='STATUS', index=['ID'], columns=['MONTHS_BALANCE'])
+    df_target.columns = ['MONTH' + str(col) for col in df_target.columns]
+    return df_target
 
 
 def _get_dummies(df: pd.DataFrame):
@@ -21,5 +40,5 @@ def _get_dummies(df: pd.DataFrame):
 if __name__ == '__main__':
     app = pd.read_csv(application_record_path)
     cred = pd.read_csv(credit_score_path)
-    cleanse_data(app)
+    cleanse_data(app, cred)
     app.to_csv(application_record_clean_path)
